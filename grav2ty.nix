a2f7ed5ca13a9d967f2455ce239e2125094b4a1d
sternenseemann <git@lukasepple.de>
2019-05-20 17:26:26 +0200

    add lens to libraryDepends

--- grav2ty.nix
+++ grav2ty.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, containers, gloss, linear, stdenv }:
+{ mkDerivation, base, containers, gloss, lens, linear, stdenv }:
 mkDerivation {
   pname = "grav2ty";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  libraryHaskellDepends = [ base containers linear ];
+  libraryHaskellDepends = [ base containers lens linear ];
   executableHaskellDepends = [ base containers gloss linear ];
   doHaddock = false;
   license = stdenv.lib.licenses.gpl3;
